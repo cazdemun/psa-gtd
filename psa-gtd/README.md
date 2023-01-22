b1c3172ad3b03a9b64f10ede6b689c6de576351d
cazdemun <cplu1509@gmail.com>
2023-01-22 15:52:29 -0500

    feat: add nedb-promises usage example

--- psa-gtd/README.md
+++ psa-gtd/README.md
@@ -1,9 +1,19 @@
 # Set up
 
     npx create-react-app psa-gtd --template typescript
+
+# Nedb set up
+
     npm i nedb-promises
 
-Follow [guide](https://www.alchemy.com/blog/how-to-polyfill-node-core-modules-in-webpack-5) for polyfills
+Follow [guide](https://www.alchemy.com/blog/how-to-polyfill-node-core-modules-in-webpack-5) for nedb polyfills
 
     npm install --save-dev react-app-rewired
     npm install --save-dev path-browserify buffer process
+
+Note: It is not clear if buffer and process are actually needed
+
+Documentation about nedb-promises can be found here:
+
+- [npm](https://www.npmjs.com/package/nedb-promises)
+- [docs](https://github.com/bajankristof/nedb-promises/master/docs.md)
--- psa-gtd/src/App.tsx
+++ psa-gtd/src/App.tsx
@@ -1,27 +1,49 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import Datastore from 'nedb-promises';
-import { trace } from './utils';
 
 type Test = {
   _id: string
   title: string
 }
 
-const datastore = Datastore.create('test');
+const datastore = Datastore.create('test') as Datastore<Test>;
 
 function App() {
   const [docs, setDocs] = useState<Test[]>([])
 
   useEffect(() => {
-    datastore.find<Test>({})
-    .then((docs) => setDocs(trace(docs)))
+    datastore.on('insert', () => {
+      datastore.find({}).then((docs) => setDocs(docs))
+    })
+
+    datastore.on('remove', () => {
+      datastore.find({}).then((docs) => setDocs(docs))
+    })
+
+    datastore.on('update', () => {
+      datastore.find({}).then((docs) => setDocs(docs))
+    })
+
+    datastore.find({}).then((docs) => setDocs(docs))
+
+    return () => {
+      datastore.removeAllListeners('insert');
+      datastore.removeAllListeners('update');
+      datastore.removeAllListeners('remove');
+    }
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        {docs.map((doc) => <div>{JSON.stringify(doc, null, 2)}</div>)}
+        <button onClick={() => datastore.insert<Omit<Test, '_id'>>({ title: 'New Test' })}>Add</button>
+        {docs.map((doc) => (
+          <div key={doc._id} style={{ display: 'flex' }}>
+            <button onClick={() => datastore.removeOne({ _id: doc._id }, {})}>Delete</button>
+            <button onClick={() => datastore.updateOne({ _id: doc._id }, { title: doc.title === 'New Test' ? 'Modified Test' : 'New Test' })}>Update</button>
+            <div>{JSON.stringify(doc, null, 2)}</div>
+          </div>
+        ))}
       </header>
     </div>
   );
