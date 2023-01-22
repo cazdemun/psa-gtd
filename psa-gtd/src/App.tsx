e391f19ab6bf958c36d687fa5e11c37e40e14cc5
cazdemun <cplu1509@gmail.com>
2023-01-22 14:51:50 -0500

    chore: install nedb-promises
    	This instalation is important because it requires polyfills

--- psa-gtd/src/App.tsx
+++ psa-gtd/src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Datastore from 'nedb-promises';
+
+type Test = {
+  _id: string
+  title: string
+}
+
+const datastore = Datastore.create('test');
+
 function App() {
+  const [docs, setDocs] = useState<Test[]>([])
+
+  useEffect(() => {
+    datastore.find<Test>({})
+    .then((docs) => setDocs(docs))
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {docs.map((doc) => <div>{JSON.stringify(doc, null, 2)}</div>)}
       </header>
     </div>
   );
