28df991f09c3a3df3d181ae3603bd2f69ed3e88d
cazdemun <cplu1509@gmail.com>
2023-02-07 12:19:59 -0500

    feat: add porjects and actions from actionable table
    this is kinda complete

--- psa-gtd/src/componentes/process/ActionableModal.tsx
+++ psa-gtd/src/componentes/process/ActionableModal.tsx
@@ -4,19 +4,156 @@ import { Button, Form, Input, Modal, Row, Select } from 'antd';
 import GlobalServicesContext from '../context/GlobalServicesContext';
 import { getLastIndexFirstLevel, getNextIndex } from '../../utils';
 import { SearchOutlined } from '@ant-design/icons';
-import { Action, Actionable, Project } from '../../models';
+import { Action, Actionable, ProcessedItem, Project } from '../../models';
 import { useForm, useWatch } from 'antd/es/form/Form';
 import { v4 as uuidv4 } from 'uuid';
 import { NewDoc } from '../../lib/Repository';
+import { ActorRefFrom } from 'xstate';
+import { ProcessedCRUDStateMachine } from '../../machines/GlobalServicesMachine';
+import { SearchSelect } from '../common/Search';
 
 const ACTIONABLE_MODAL_LABEL_COL = 2;
 
+type ActionableFormValues = Action | (Project & { description: string })
+
+const onFinish = (
+  values: ActionableFormValues,
+  lastProcessedIndex: number,
+  actionableToProcess: Actionable | undefined,
+  processedItemsMap: Map<string, ProcessedItem>,
+  ProcessedCRUDService: ActorRefFrom<ProcessedCRUDStateMachine>
+) => {
+  // console.log(generateProjectsAndActions(values.content, lastProcessedIndex));
+
+  if (values.type === 'project') {
+    const projectId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+    const nextIndex = getNextIndex(lastProcessedIndex);
+
+    const newActions: NewDoc<Action>[] = values.content
+      .split('\n')
+      .map((actionContent) => actionContent.trim())
+      .filter((actionContent) => actionContent !== undefined && actionContent !== '')
+      .map((content, i) => ({
+        _id: uuidv4(),
+        type: 'action',
+        content,
+        created: Date.now(),
+        index: `${nextIndex}.${getNextIndex(i)}`,
+        modified: Date.now(),
+        project: projectId,
+      }))
+
+    const newProject: NewDoc<Project> = {
+      _id: projectId,
+      type: 'project',
+      content: values.description ?? '',
+      project: values.project ?? undefined,
+      created: Date.now(),
+      index: nextIndex,
+      modified: Date.now(),
+      actions: newActions.map((newAction) => newAction._id as string),
+      title: values.title,
+    }
+
+    const parentProject = processedItemsMap.get(values.project ?? '') as Project | undefined;
+
+    if (parentProject) {
+      const updatedParentProject: Partial<Project> = {
+        actions: [...new Set([...parentProject.actions, projectId])],
+        modified: Date.now(),
+      }
+      ProcessedCRUDService.send({
+        type: 'BATCH',
+        data: [
+          {
+            type: 'CREATE',
+            doc: [newProject, ...newActions],
+          }, {
+            type: 'UPDATE',
+            _id: parentProject._id,
+            doc: updatedParentProject,
+          }, {
+            type: 'DELETE',
+            _id: actionableToProcess?._id,
+          }
+        ]
+      })
+    } else {
+      ProcessedCRUDService.send({
+        type: 'BATCH',
+        data: [
+          {
+            type: 'CREATE',
+            doc: [newProject, ...newActions],
+          }, {
+            type: 'DELETE',
+            _id: actionableToProcess?._id,
+          }
+        ]
+      })
+    }
+  }
+  if (values.type === 'action') {
+    const actionId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+    const nextIndex = getNextIndex(lastProcessedIndex);
+
+    const newAction: NewDoc<Action> = {
+      _id: actionId,
+      type: 'action',
+      content: values.content,
+      project: values.project ?? undefined,
+      created: Date.now(),
+      index: nextIndex,
+      modified: Date.now(),
+    }
+
+    const parentProject = processedItemsMap.get(values.project ?? '') as Project | undefined;
+
+    if (parentProject) {
+      const updatedParentProject: Partial<Project> = {
+        actions: [...new Set([...parentProject.actions, actionId])],
+        modified: Date.now(),
+      }
+      ProcessedCRUDService.send({
+        type: 'BATCH',
+        data: [
+          {
+            type: 'CREATE',
+            doc: newAction,
+          }, {
+            type: 'UPDATE',
+            _id: parentProject._id,
+            doc: updatedParentProject,
+          }, {
+            type: 'DELETE',
+            _id: actionableToProcess?._id,
+          }
+        ]
+      })
+    } else {
+      ProcessedCRUDService.send({
+        type: 'BATCH',
+        data: [
+          {
+            type: 'CREATE',
+            doc: newAction,
+          }, {
+            type: 'DELETE',
+            _id: actionableToProcess?._id,
+          }
+        ]
+      })
+    }
+  }
+}
+
 type DestroyableFormProps = {
   actionableToProcess: Actionable | undefined,
+  onFinish: (...args: any[]) => any
 }
 
 const DestroyableForm: React.FC<DestroyableFormProps> = (props) => {
-  const [form] = useForm<Action | (Project & { description: string })>();
+  const [form] = useForm<ActionableFormValues>();
   const { type, ...initialValues } = props.actionableToProcess || {};
 
   const actionType = useWatch('type', form);
@@ -25,6 +162,7 @@ const DestroyableForm: React.FC<DestroyableFormProps> = (props) => {
 
   const ProcessedCRUDService = useSelector(service, ({ context }) => context.processedCRUDActor);
   const processedItems = useSelector(ProcessedCRUDService, ({ context }) => context.docs);
+  const processedItemsMap = useSelector(ProcessedCRUDService, ({ context }) => context.docsMap);
 
   const lastProcessedIndex = getLastIndexFirstLevel(processedItems);
 
@@ -34,50 +172,8 @@ const DestroyableForm: React.FC<DestroyableFormProps> = (props) => {
       labelCol={{ span: ACTIONABLE_MODAL_LABEL_COL }}
       initialValues={{ ...initialValues, type: 'project' }}
       onFinish={(values) => {
-        // console.log(generateProjectsAndActions(values.content, lastProcessedIndex));
-
-        if (values.type === 'project') {
-          const projectId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
-          const nextIndex = getNextIndex(lastProcessedIndex);
-
-          const newActions: NewDoc<Action>[] = values.content
-            .split('\n')
-            .map((actionContent) => actionContent.trim())
-            .filter((actionContent) => actionContent !== undefined && actionContent !== '')
-            .map((content, i) => ({
-              _id: uuidv4(),
-              type: 'action',
-              content,
-              created: Date.now(),
-              index: `${nextIndex}.${getNextIndex(i)}`,
-              modified: Date.now(),
-              project: projectId,
-            }))
-
-          const newProject: NewDoc<Project> = {
-            _id: projectId,
-            type: 'project',
-            content: values.description ?? '',
-            created: Date.now(),
-            index: nextIndex,
-            modified: Date.now(),
-            actions: newActions.map((newAction) => newAction._id as string),
-            title: values.title
-          }
-
-          ProcessedCRUDService.send({
-            type: 'BATCH',
-            data: [
-              {
-                type: 'CREATE',
-                doc: [newProject, ...newActions],
-              }, {
-                type: 'DELETE',
-                _id: props.actionableToProcess?._id,
-              }
-            ]
-          })
-        }
+        onFinish(values, lastProcessedIndex, props.actionableToProcess, processedItemsMap, ProcessedCRUDService);
+        props.onFinish();
       }}
     >
       <Form.Item
@@ -115,11 +211,22 @@ const DestroyableForm: React.FC<DestroyableFormProps> = (props) => {
       <Row align='top'>
         <Form.Item
           label="Parent"
+          name='project'
           labelCol={{ span: ACTIONABLE_MODAL_LABEL_COL }}
           wrapperCol={{ span: 21 }}
           style={{ flex: '1' }}
         >
-          <Input disabled />
+          <SearchSelect
+            showSearch
+            options={processedItems
+              .filter((doc): doc is Project => doc.type === 'project')
+              .sort((a, b) => b.modified - a.modified)
+              .map((doc) => ({
+                label: doc.title,
+                value: doc._id,
+              }))
+            }
+          />
         </Form.Item>
         <div>
           <Button icon={<SearchOutlined />} />
@@ -143,7 +250,11 @@ const DestroyableForm: React.FC<DestroyableFormProps> = (props) => {
           <Input disabled />
         </Form.Item>
       </Row>
-      <Form.Item label={actionType === 'action' ? "Title" : "Actions"} name='content'>
+      <Form.Item
+        label={actionType === 'action' ? "Title" : "Actions"}
+        name='content'
+        rules={actionType === 'action' ? [{ required: true, message: 'Please add some text' }] : []}
+      >
         <Input.TextArea
           autoSize={{ minRows: 10 }}
         />
@@ -177,7 +288,10 @@ const ActionableModal: React.FC<ActionableModalProps> = (props) => {
       onOk={props.onOk}
       destroyOnClose
     >
-      <DestroyableForm actionableToProcess={props.actionableToProcess} />
+      <DestroyableForm
+        actionableToProcess={props.actionableToProcess}
+        onFinish={props.onCancel}
+      />
       <pre>
         {JSON.stringify(props.actionableToProcess, null, 2)}
       </pre>
